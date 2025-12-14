55fcc81e7c1863343026f1555856592efa72c9db
tranquang20042004-arch <tranquang20042004@gmail.com>
2025-12-14 10:20:37 +0700

    ADD: tao form them sinh vien Refs #32

---
+++ QuanLySinhVien/app/Models/Student.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'students';
+
+    protected $primaryKey = 'student_id';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'student_id',
+        'full_name',
+        'birth_date',
+        'avatar',
+        'class_id',
+    ];
+
+    protected $casts = [
+        'birth_date' => 'date',
+    ];
+
+    public function classRelation()
+    {
+        return $this->belongsTo(ClassModel::class, 'class_id', 'class_id');
+    }
+}
+
